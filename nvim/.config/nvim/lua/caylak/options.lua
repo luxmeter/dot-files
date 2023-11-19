c7c4ce0496430d687b8d38d3f4506ae283aa2229
Mustafa Caylak <mustafa.caylak@web.de>
2023-11-19 14:17:31 +0100

    update

--- nvim/.config/nvim/lua/caylak/options.lua
+++ nvim/.config/nvim/lua/caylak/options.lua
@@ -25,10 +25,10 @@ o.encoding = "utf-8"
 o.encoding = "utf8"
 o.expandtab = true -- always use spaces instead of tabs
 o.fillchars = {
-	diff = "∙", -- BULLET OPERATOR (U+2219, UTF-8: E2 88 99)
-	eob = " ", -- NO-BREAK SPACE (U+00A0, UTF-8: C2 A0) to suppress ~ at EndOfBuffer
-	fold = "·", -- MIDDLE DOT (U+00B7, UTF-8: C2 B7)
-	vert = "┃", -- BOX DRAWINGS HEAVY VERTICAL (U+2503, UTF-8: E2 94 83)
+  diff = "∙", -- BULLET OPERATOR (U+2219, UTF-8: E2 88 99)
+  eob = " ", -- NO-BREAK SPACE (U+00A0, UTF-8: C2 A0) to suppress ~ at EndOfBuffer
+  fold = "·", -- MIDDLE DOT (U+00B7, UTF-8: C2 B7)
+  vert = "┃", -- BOX DRAWINGS HEAVY VERTICAL (U+2503, UTF-8: E2 94 83)
 }
 o.foldlevelstart = 99 -- start unfolded
 o.foldmethod = "indent" -- not as cool as syntax, but faster
@@ -44,11 +44,11 @@ o.lazyredraw = true -- don't bother updating screen during macro playback
 o.linebreak = true -- wrap long lines at characters in 'breakat'
 o.list = true -- show whitespace
 o.listchars = {
-	nbsp = "⦸", -- CIRCLED REVERSE SOLIDUS (U+29B8, UTF-8: E2 A6 B8)
-	extends = "»", -- RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00BB, UTF-8: C2 BB)
-	precedes = "«", -- LEFT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00AB, UTF-8: C2 AB)
-	tab = "▷⋯", -- WHITE RIGHT-POINTING TRIANGLE (U+25B7, UTF-8: E2 96 B7) + MIDLINE HORIZONTAL ELLIPSIS (U+22EF, UTF-8: E2 8B AF)
-	trail = "•", -- BULLET (U+2022, UTF-8: E2 80 A2)
+  nbsp = "⦸", -- CIRCLED REVERSE SOLIDUS (U+29B8, UTF-8: E2 A6 B8)
+  extends = "»", -- RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00BB, UTF-8: C2 BB)
+  precedes = "«", -- LEFT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00AB, UTF-8: C2 AB)
+  tab = "▷⋯", -- WHITE RIGHT-POINTING TRIANGLE (U+25B7, UTF-8: E2 96 B7) + MIDLINE HORIZONTAL ELLIPSIS (U+22EF, UTF-8: E2 8B AF)
+  trail = "•", -- BULLET (U+2022, UTF-8: E2 80 A2)
 }
 o.modelines = 5 -- scan this many lines looking for modeline
 o.mouse = "a" -- enable mouse movement - makes copy & paste hard to use
@@ -117,21 +117,21 @@ vim.cmd("colorscheme nightfox")
 -- ]]
 
 if vim.fn.executable("rg") then
-	local grep = {
-		"rg",
-		"--vimgrep",
-		"--no-heading",
-		"--smart-case",
-		"--hidden",
-		"--follow",
-		"--no-ignore-global",
-		"--ignore-file",
-		vim.env.HOME .. "/.config/nvim/ignore-patterns",
-	}
-	local join = require("plenary.functional").join
-	local result = join(grep, " ")
-	o.grepprg = result
-	o.grepformat = "%f:%l:%c:%m"
+  local grep = {
+    "rg",
+    "--vimgrep",
+    "--no-heading",
+    "--smart-case",
+    "--hidden",
+    "--follow",
+    "--no-ignore-global",
+    "--ignore-file",
+    vim.env.HOME .. "/.config/nvim/ignore-patterns",
+  }
+  local join = require("plenary.functional").join
+  local result = join(grep, " ")
+  o.grepprg = result
+  o.grepformat = "%f:%l:%c:%m"
 end
 
 -- navigate via option arrow keys in macos within tmux
